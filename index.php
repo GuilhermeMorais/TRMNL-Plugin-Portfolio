6434e4f7442f104d78a2981049e35959a1971382
Jasper van der Meij <jvdmeij@users.noreply.github.com>
2026-01-11 11:07:05 +0100

    Fix formatting in index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 $userId = '2633'; // Enter your TRMNL userId, can be found in plugin variable `trmnl.user.id`
-$siteName = 'TRMNL Plugins';
+$siteName = 'TRMNL Plugins'; // For example `TRMNL Plugins by Jasper`
 $defaultColorMode = 'light'; // Options: 'light' or 'dark'
 $debug = false; // Set to false to disable logging to debug.log
 
